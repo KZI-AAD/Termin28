9f3baa3ecb2e7238fd04acaf0d4585177128631e
milossimic <milossimicsimo@gmail.com>
2017-03-06 16:21:37 +0100

    Termin28

---
+++ app/src/main/java/rs/aleph/android/example28/fragments/MapFragment.java
@@ -0,0 +1,247 @@
+package rs.aleph.android.example28.fragments;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import rs.aleph.android.example28.R;
+import rs.aleph.android.example28.dialogs.LocationDialog;
+
+/**
+ * Created by milossimic on 4/13/16.
+ */
+/**
+ * Fragment koji treab da prikaze mapu treba da implementira i OnMapReadyCallback
+ * interface posto su nove verzije mape asinhrone, tj ucitavanje mape se oddvija asinhrono.
+ *
+ * Cesto je slucaj da implementira i LocationListener da bi mogli odmah da dobijamo
+ * i informacije i lokaciji
+ * */
+public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback {
+
+    private LocationManager locationManager;
+    private String provider;
+    private SupportMapFragment mMapFragment;
+    private AlertDialog dialog;
+    private Marker home;
+    private GoogleMap map;
+
+    public static MapFragment newInstance() {
+
+        MapFragment mpf = new MapFragment();
+
+        return mpf;
+    }
+
+    /**
+     * Prilikom kreidanja fragmenta preuzimamo sistemski servis za rad sa lokacijama
+     * */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+    }
+
+    /**
+     * Kada zelmo da dobijamo informacije o lokaciji potrebno je da specificiramo
+     * po kom kriterijumu zelimo da dobijamo informacije GSP, MOBILNO(WIFI, MObilni internet), GPS+MOBILNO
+     * **/
+    private void createMapFragmentAndInflate() {
+        //specificiramo krijterijum da dobijamo informacije sa svih izvora
+        //ako korisnik to dopusti
+        Criteria criteria = new Criteria();
+
+        //sistemskom servisu prosledjujemo taj kriterijum da bi
+        //mogli da dobijamo informacje sa tog izvora
+        provider = locationManager.getBestProvider(criteria, true);
+
+        //kreiramo novu instancu fragmenta
+        mMapFragment = SupportMapFragment.newInstance();
+
+        //i vrsimo zamenu trenutnog prikaza sa prikazom mape
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.map_container, mMapFragment).commit();
+
+        //pozivamo ucitavnje mape.
+        //VODITI RACUNA OVO JE ASINHRONA OPERACIJA
+        //LOKACIJE MOGU DA SE DOBIJU PRE MAPE I OBRATNO
+        mMapFragment.getMapAsync(this);
+    }
+
+
+    private void showLocatonDialog(){
+        if(dialog == null){
+            dialog = new LocationDialog(getActivity()).prepareDialog();
+        }else{
+            if(dialog.isShowing()){
+                dialog.dismiss();
+            }
+        }
+
+        dialog.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        createMapFragmentAndInflate();
+
+        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean wifi = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!gps || !wifi){
+            showLocatonDialog();
+        } else {
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle data) {
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.map_layout, vg, false);
+
+        return view;
+    }
+
+    /**
+     * Svaki put kada uredjaj dobijee novu informaciju o lokaciji ova metoda se poziva
+     * i prosledjuje joj se nova informacija o kordinatamad
+     * */
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getActivity(), "Toast", Toast.LENGTH_SHORT).show();
+        if (map != null) {
+            addMarker(location);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    /**
+     * KAda je mapa spremna mozemo da radimo sa njom.
+     * Mozemo reagovati na razne dogadjaje dodavanje markera, pomeranje markera,klik na mapu,...
+     * */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        map = googleMap;
+
+        //ako zelimo da rucno postavljamo markere to radimo
+        //dodavajuci click listener
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                map.addMarker(new MarkerOptions()
+                        .title("YOUR_POSITON")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                        .position(latLng));
+                home.setFlat(true);
+
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng).zoom(14).build();
+
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        //ako zelmo da reagujemo na klik markera koristimo marker click listener
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getActivity(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        //ako je potrebno da reagujemo na pomeranje markera koristimo marker drag listener
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                Toast.makeText(getActivity(), "Drag started", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                Toast.makeText(getActivity(), "Dragging", Toast.LENGTH_SHORT).show();
+                map.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                Toast.makeText(getActivity(), "Drag ended", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if (location != null) {
+            addMarker(location);
+        }
+    }
+
+    private void addMarker(Location location) {
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if (home != null) {
+            home.remove();
+        }
+
+        home = map.addMarker(new MarkerOptions()
+                .title("YOUR_POSITON")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .position(loc));
+        home.setFlat(true);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(loc).zoom(14).build();
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    /**
+     *
+     * Rad sa lokacja izuzetno trosi bateriju.Obavezno osloboditi kada vise ne koristmo
+     * */
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
+    }
+}
